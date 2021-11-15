390a54dc5c09c7f7cf91cadc2bde56350c3c8326
Erwan Le Goff <erwan.legoff@compufirst.com>
2021-11-15 16:27:24 +0100

    New project

---
+++ src/projet/ProjetTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet.test.first;
+
+/**
+ *
+ * @author erwan.legoff
+ */
+public class ProjetTestFirst {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
