bbd785d2f700d39527af81c3f694c614bed5e2b6
Erwan Le Goff <erwan.legoff@compufirst.com>
2021-11-15 17:13:07 +0100

    add calculatrice and tests

---
+++ src/projet/test/first/Calculatrice.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet.test.first;
+
+/**
+ *
+ * @author erwan.legoff
+ */
+public class Calculatrice {
+    public static double sum(double a, double b){
+        return a + b;
+    }
+    public static double multiply(double a, double b){
+        return a * b;
+    }
+    public static double divide(double a, double b){
+        return a / b;
+    }
+    public static double substract(double a, double b){
+        return a - b;
+    }
+}
---
+++ test/projet/test/first/CalculatriceTest.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet.test.first;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author erwan.legoff
+ */
+public class CalculatriceTest {
+    
+    public CalculatriceTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of sum method, of class Calculatrice.
+     */
+    @Test
+    public void testSum() {
+        System.out.println("sum");
+        double a = 0.0;
+        double b = 1.0;
+        double expResult = 1.0;
+        double result = Calculatrice.sum(a, b);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of multiply method, of class Calculatrice.
+     */
+    @Test
+    public void testMultiply() {
+        System.out.println("multiply");
+        double a = 1.0;
+        double b = 0.0;
+        double expResult = 0.0;
+        double result = Calculatrice.multiply(a, b);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of divide method, of class Calculatrice.
+     */
+    @Test
+    public void testDivide() {
+        System.out.println("divide");
+        double a = 0.0;
+        double b = 1.0;
+        double expResult = 0.0;
+        double result = Calculatrice.divide(a, b);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of substract method, of class Calculatrice.
+     */
+    @Test
+    public void testSubstract() {
+        System.out.println("substract");
+        double a = 1.0;
+        double b = 1.0;
+        double expResult = 0.0;
+        double result = Calculatrice.substract(a, b);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
